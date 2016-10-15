2b3d054516780a99e1b15e595ac2412f2cfa53dd
Jussi Pakkanen <jpakkane@gmail.com>
2016-10-15 16:27:23 +0300

    Do package modification steps.

--- dist.py
+++ dist.py
@@ -15,7 +15,8 @@
 # limitations under the License.
 
 import argparse, sys, os, shutil
-import subprocess, tarfile
+import subprocess, tarfile, zipfile
+from collections import namedtuple
 
 parser = argparse.ArgumentParser()
 
@@ -23,6 +24,22 @@ parser.add_argument('--name', default='emptyproject-0', dest='name',
                     help='Name to use in leading directory and package file names.')
 parser.add_argument('--distfile', default=None, dest='distfile',
                     help='Distribution description file.')
+
+def parse_distfile(dfname):
+    Dist = namedtuple('Dist', ['copy', 'delete', 'run'])
+    return Dist(['extra_file'], ['subdir/removeme.txt'], ['./packagingscript.py'])
+
+def create_zip(zipfilename, packaging_dir):
+    zf = zipfile.ZipFile(zipfilename, 'w', compression=zipfile.ZIP_DEFLATED)
+    zf.write(packaging_dir)
+    for root, dirs, files in os.walk(packaging_dir):
+        for d in dirs:
+            dname = os.path.join(root, d)
+            zf.write(dname, dname)
+        for f in files:
+            fname = os.path.join(root, f)
+            zf.write(fname, fname)
+    zf.close()
 
 def create_dist(name, distfile):
     packaging_dir = name
@@ -38,10 +55,28 @@ def create_dist(name, distfile):
         os.unlink(os.path.join(packaging_dir, '.gitignore'))
     except Exception:
         pass
+    dist_info = parse_distfile(distfile)
+    for c in dist_info.copy:
+        ofname = os.path.join(packaging_dir, c)
+        if os.path.isfile(c):
+            shutil.copy2(c, ofname)
+        else:
+            shutil.copytree(c, ofname)
+    for d in dist_info.delete:
+        dfname = os.path.join(packaging_dir, d)
+        if os.path.isfile(dfname):
+            os.unlink(dfname)
+        else:
+            shutil.rmtree(dfname)
+    for script in dist_info.run:
+        subprocess.check_call([script, packaging_dir])
     tarfname = name + '.tar.xz'
     tf = tarfile.open(tarfname, 'w:xz')
     tf.add(packaging_dir, packaging_dir)
     tf.close()
+    zipfilename = name + '.zip'
+    create_zip(zipfilename, packaging_dir)
+    # Create checksums here?
     shutil.rmtree(packaging_dir)
 
 if __name__ == '__main__':
@@ -55,4 +90,6 @@ if __name__ == '__main__':
     if not os.path.exists('.git'):
         print('No .git directory in current directory, can not create distribution.')
         sys.exit(1)
+    open('extra_file', 'w').close()
     create_dist(options.name, options.distfile)
+    os.unlink('extra_file')
